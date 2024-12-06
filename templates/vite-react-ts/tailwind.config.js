058b1e44c0540721d70e16f526fdd75e6d82f49c
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2024-12-07 03:27:52 +0530

    updated

--- templates/vite-react-ts/tailwind.config.js
+++ templates/vite-react-ts/tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/xtrixui/dist/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode: "class",
   theme: {
     extend: {},
   },
