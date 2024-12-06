c2ecc8499f1e9f719003ffb5fe6dc62f966ad069
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2024-12-07 02:55:32 +0530

    updated

--- src/Frameworks.ts
+++ src/Frameworks.ts
@@ -1,7 +1,7 @@
 export const frameworks = {
   "next-app-js": {
     name: "Next.js (App Router + JavaScript)",
-    templatePath: "Templates/nextjs-app-js",
+    templatePath: "templates/nextjs-app-js",
     scripts: {
       dev: "next dev --turbopack",
       build: "next build",
@@ -22,7 +22,57 @@ export const frameworks = {
   },
   "next-app-ts": {
     name: "Next.js (App Router + TypeScript)",
-    templatePath: "Templates/nextjs-app-ts",
+    templatePath: "templates/nextjs-app-ts",
+    scripts: {
+      dev: "next dev --turbopack",
+      build: "next build",
+      start: "next start",
+      lint: "next lint",
+    },
+    dependencies: {
+      next: "latest",
+      react: "latest",
+      "react-dom": "latest",
+    },
+    devDependencies: {
+      tailwindcss: "latest",
+      typescript: "latest",
+      "@types/node": "latest",
+      "@types/react": "latest",
+      "@types/react-dom": "latest",
+      postcss: "latest",
+      eslint: "latest",
+      "eslint-config-next": "latest",
+    },
+  },
+  "vite-react": {
+    name: "Vite + React + SWC + JavaScript",
+    templatePath: "templates/nextjs-app-ts",
+    scripts: {
+      dev: "next dev --turbopack",
+      build: "next build",
+      start: "next start",
+      lint: "next lint",
+    },
+    dependencies: {
+      next: "latest",
+      react: "latest",
+      "react-dom": "latest",
+    },
+    devDependencies: {
+      tailwindcss: "latest",
+      typescript: "latest",
+      "@types/node": "latest",
+      "@types/react": "latest",
+      "@types/react-dom": "latest",
+      postcss: "latest",
+      eslint: "latest",
+      "eslint-config-next": "latest",
+    },
+  },
+  "vite-react-ts": {
+    name: "Vite + React + SWC + TypeScript",
+    templatePath: "templates/nextjs-app-ts",
     scripts: {
       dev: "next dev --turbopack",
       build: "next build",
